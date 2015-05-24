58fef812d6b600ff462366f6b628475076dd4f02
JC/BC <analytalica@gmail.com>
2015-05-24 16:29:38 -0500

    Removed old sound proof of concept implementation.

--- src/js/main.js
+++ src/js/main.js
@@ -513,20 +513,6 @@ var Module = React.createClass({
             (this.verifyTimerMaxFields(true) && label != '' && (labelToId[label] == this.props.id || !labelToId[label])); //check if all valid
     },
 
-    playSound_bloop_d: function () { //Optional: pass in an event parameter
-        if (this.props.soundEnabled) {
-            //document.getElementById('sounds_bloop_d').play()
-            createjs.Sound.play('bloop_d');
-        }
-    },
-
-    playSound_bloop_g: function () {
-        if (this.props.soundEnabled) {
-            //document.getElementById('sounds_bloop_g').play()
-            createjs.Sound.play('bloop_g');
-        }
-    },
-
     render: function () {
         var upToDownFrom;
         //Set countUp/countDown limit text
@@ -668,8 +654,6 @@ var Module = React.createClass({
                                     </tr>
                                 </table>
                             </div>
-                            <button onClick={this.playSound_bloop_g}>Play a Bloop G</button>
-                            <button onClick={this.playSound_bloop_d}>Play a Bloop D</button>
                         </div>
                     </div>
                 </div>
