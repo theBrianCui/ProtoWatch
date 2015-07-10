7ee95b550b60d218b2d28dd4884f5536fc09f9a4
JC/BC <analytalica@gmail.com>
2015-07-09 23:11:25 -0500

    Added build script

---
+++ build.ps1
@@ -0,0 +1,11 @@
+git branch -f staging
+cp -r .\src\* .\build\
+rm .\build\js -Recurse
+jsx src\js\ build\js\
+rm .\build\js\JSXTransformer-0.13.0.js
+rm .\build\js\react-with-addons-0.13.0.js
+rm .\build\js\.module-cache -Recurse
+(Get-Content .\build\index.html) | 
+Foreach-Object {$_ -replace 'react-with-addons-0.13.0.js','react-with-addons-0.13.0-production.js'}  | 
+Out-File .\build\js\main.js
+git checkout master
