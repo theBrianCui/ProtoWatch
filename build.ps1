4a82a54e191405e7809e17718b4e67c7cd6de2d8
JC/BC <analytalica@gmail.com>
2015-07-09 23:17:43 -0500

    Modified build script

--- build.ps1
+++ build.ps1
@@ -8,4 +8,5 @@ rm .\build\js\.module-cache -Recurse
 (Get-Content .\build\index.html) | 
 Foreach-Object {$_ -replace 'react-with-addons-0.13.0.js','react-with-addons-0.13.0-production.js'}  | 
 Out-File .\build\js\main.js
+git commit -a -m "Build commit for $(get-date -format s)"
 git checkout master
