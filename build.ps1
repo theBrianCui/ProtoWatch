0e1fa26852fa51d6588ee8ececedc9bbb32f29b2
JC/BC <analytalica@gmail.com>
2015-07-09 23:24:13 -0500

    Updated build script

--- build.ps1
+++ build.ps1
@@ -1,3 +1,4 @@
+rm .\build\ -Recurse
 git branch -f staging
 cp -r .\src\* .\build\
 rm .\build\js -Recurse
@@ -8,7 +9,4 @@ rm .\build\js\.module-cache -Recurse
 (Get-Content .\build\index.html) | 
 Foreach-Object {$_ -replace 'react-with-addons-0.13.0.js','react-with-addons-0.13.0-production.js'}  | 
 Out-File .\build\js\main.js
-git commit -a -m "Build commit for $(get-date -format s)"
-git checkout master
 rm .\build\ -Recurse
-git reset --hard
