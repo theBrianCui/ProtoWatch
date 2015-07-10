8d162d9e8cd88c26845d478f414740510cf1a734
JC/BC <analytalica@gmail.com>
2015-07-09 23:45:25 -0500

    Updated build config

--- build.ps1
+++ build.ps1
@@ -11,7 +11,8 @@ rm .\build\js\.module-cache -Recurse
 Where-Object {$_ -notmatch 'JSXTransformer'} |
 Foreach-Object {$_ -replace 'react-with-addons-0.13.0.js','react-with-addons-0.13.0-production.js'}  | 
 Foreach-Object {$_ -replace ' type="text/jsx"',''}  | 
-Out-File .\build\index.html
+$output = $_
+Out-File $output
 git add .
 git commit -a -m "Build commit for $(get-date -format s)"
 git checkout master
